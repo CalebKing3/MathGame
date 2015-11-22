66f646d2f7027942b351ee45fe46c8a21c27170c
Caleb King <CalebKing@LiveNation.com>
2015-11-21 23:36:57 -0700

    Connected the main page with the gameactivity

--- app/src/main/java/calebking/me/mathgamechapter2/MainActivity.java
+++ app/src/main/java/calebking/me/mathgamechapter2/MainActivity.java
@@ -1,18 +1,30 @@
 package calebking.me.mathgamechapter2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonPlay = (Button)findViewById(R.id.buttonPlay);
+        buttonPlay.setOnClickListener(this);
+
+
+
+
+
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -24,5 +36,11 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i = new Intent(this, GameActivity.class);
+        startActivity(i);
     }
 }
