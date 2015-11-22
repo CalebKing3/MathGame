8d75f4dcb9804dac7e26c02bd16db17adf9f2c13
Caleb King <CalebKing@LiveNation.com>
2015-11-21 23:08:54 -0700

    Connected buttons to gameactivity

--- app/src/main/java/calebking/me/mathgamechapter2/GameActivity.java
+++ app/src/main/java/calebking/me/mathgamechapter2/GameActivity.java
@@ -6,13 +6,45 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_game);
+
+        int partA = 9;
+        int partB = 9;
+        int correctAnswer = partA * partB;
+        int wrongAnswer1 = correctAnswer - 1;
+        int wrongAnswer2 = correctAnswer + 1;
+
+        TextView textObjectPartA = (TextView)findViewById(R.id.textPartA);
+        TextView textObjectPartB = (TextView)findViewById(R.id.textPartB);
+
+        Button buttonObjectChoice1 = (Button)findViewById(R.id.buttonChoice1);
+        Button buttonObjectChoice2 = (Button)findViewById(R.id.buttonChoice2);
+        Button buttonObjectChoice3 = (Button)findViewById(R.id.buttonChoice3);
+
+
+        textObjectPartA.setText("" + partA);
+        textObjectPartB.setText("" + partB);
+
+        //which button receives which answer, at this stage is arbitrary.
+
+        buttonObjectChoice1.setText("" + correctAnswer);
+        buttonObjectChoice2.setText("" + wrongAnswer1);
+        buttonObjectChoice3.setText("" + wrongAnswer2);
+
+
+
+
+
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
